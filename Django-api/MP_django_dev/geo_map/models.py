859cb47b01710bae3a281f0348673880a5716340
TemporalData <63362559+TemporalData@users.noreply.github.com>
2021-01-27 16:50:47 +0100

    postgis database

--- Django-api/MP_django_dev/geo_map/models.py
+++ Django-api/MP_django_dev/geo_map/models.py
@@ -1,13 +1,15 @@
-from django.db import models
-
 # Import for postgres ArrayField
 from django.contrib.postgres.fields import ArrayField
+
+# Import for the postgis elements in the db
+from django.contrib.gis.db import models
 
 # Create your models here.
 
 
 class GeoData(models.Model):
     tweet_id = models.IntegerField(null=False)
+    location = models.PointField(null=True, default=None)
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
 
