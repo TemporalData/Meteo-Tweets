a34bd15a41f7c4f861bdeb85cb180c894d9a8d1f
TemporalData <63362559+TemporalData@users.noreply.github.com>
2021-01-29 22:46:05 +0100

    Modified the geo model and api
    About 80% complete

--- Django-api/MP_django_dev/api/views/geo_view.py
+++ Django-api/MP_django_dev/api/views/geo_view.py
@@ -3,9 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 # Import the model from the app geo map
-from geo_map.models import GeoData, GeoCache
-# Import the serializer from the app geo map
-from geo_map.serializers import GeoDataSerializer, GeoCacheSerializer
+from geo_map.models import GeoCache, GeoData, GeoLocation
 # Import function from processing.py from the app geo map
 from geo_map.processing import compute_map_data
 
@@ -34,9 +32,11 @@ class GeoDataAPI(APIView):
         try:
             # Load the list in the request into id_filter variable
             id_filter = request.query_params['id_filter']
+            geoloc_filter = request.query_params['geoloc_filter']
         # Catch exceptions caused by 'id_filter' not being in the request
         except Exception:
-            # Return a bad request status due to lacking 'id_filter
+            # Return a bad request status due to lacking
+            # 'id_filter' or 'geoloc_filter'
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
         # Try to retrieve the data from the Data model
@@ -47,30 +47,67 @@ class GeoDataAPI(APIView):
                 id_filter = list(map(int, id_filter.split(',')))
 
                 # Load the filtered data into the 'data' variable
-                data = GeoData.objects.filter(id__in=id_filter)
+                data = GeoData.objects.filter(id__in=id_filter).values_list(
+                    'geo_location_id',
+                    flat=True)
             # 'id_filter' is empty list
             else:
-                # Set 'data' to all the entries in the Data model
-                data = GeoCache.objects.all().values_list(
-                    'data_list',
+                data = GeoData.objects.all().values_list(
+                    'geo_location_id',
                     flat=True)
+            # else:
+            #     # Set 'data' to all the entries in the Data model
+            #     data = GeoCache.objects.all().values_list(
+            #         'data_list',
+            #         flat=True)
 
-                data_response = pd.DataFrame(np.array(data).T)
+            #     data_response = pd.DataFrame(np.array(data).T)
 
-                data_response.columns = ['latitude', 'longitude', 'density']
+            #     data_response.columns = ['latitude', 'longitude', 'density']
 
-                return Response(data_response)
+            #     return Response(data_response)
 
         # If the Data model does not exist
         except GeoData.DoesNotExist:
             # Return a internal server error, as Data model isn't populated
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        # Store the latitude and longitude data in a variable
-        latLongData = (pd.DataFrame(data.values_list())).iloc[:, [2, 3]]
+        densities = np.unique(np.array(data), return_counts=True)
 
-        # Call compute map data with the lat and long data
-        map_data = compute_map_data(latLongData)
+        if len(geoloc_filter) > 0:
+            geo_locations, densities_index = np.intersect1d(
+                densities[0],
+                geoloc_filter,
+                return_indices=True)[:2]
+
+            geoloc_densities = densities[1][densities_index]
+        else:
+            geo_locations = densities[0]
+            geoloc_densities = densities[1]
+
+        print(geo_locations)
+        print(geoloc_densities)
+
+        # Retrieve the lat long coordinates
+        try:
+
+            latLongData = GeoLocation.objects.filter(
+                id__in=geo_locations).values_list('latitude', 'longitude')
+
+            print("Achieved")
+
+        # If the Data model does not exist
+        except GeoLocation.DoesNotExist:
+            # Return a internal server error, as Data model isn't populated
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        print(np.array(latLongData))
+
+        # Call compute colors of map data with the lat and long data
+        color_map_data = compute_map_data(latLongData)
+
+        # Add the IDs to the dataframe
+        color_map_data["id"] = dfData.iloc[:, 1]
 
         # Return the map data
-        return Response(map_data)
+        return Response(color_map_data)
