ec4ae0dffc8d4c5bfb7ac8ba0571d48487df9a82
TemporalData <63362559+TemporalData@users.noreply.github.com>
2021-04-08 22:15:53 +0200

    Update geo_pipeline.py

--- Preprocessing/geo_pipeline.py
+++ Preprocessing/geo_pipeline.py
@@ -2,33 +2,54 @@
 import pandas as pd
 import numpy as np
 import os
+from tqdm import tqdm
 
 #  Dataset name and directories
 FILE_DIR = os.path.join(os.getcwd(), 'data')  # Directory to save output
 DATAFILE = 'cleaned_dataset.csv'  # Original dataset
 
 
-def generate_geo_map(filedir, filename):
+def generate_geo_data(filedir, filename):
+
+    # Declare a progress bar
+    pbar = tqdm(total=7)
+
     # Read, slice data
     raw = pd.read_csv(
         os.path.join(filedir, filename),
         engine='python',
         encoding='latin_1')
 
-    cols = ['latitude', 'longitude']  # index as "doc_no", i.e. tweet id
+    pbar.update(1)
+
+    # Columns that the geo model requires
+    cols = ['latitude', 'longitude']
 
     partial = raw.loc[:, cols]
+    pbar.update(1)
 
     # Set original index as tweet id
-    partial['doc_no'] = partial.index
+    partial['ID'] = partial.index
+    pbar.update(1)
 
-    print(partial.head())
+    partial = partial.loc[:, ['ID']+cols]
+    pbar.update(1)
+
+    partial.loc[:, 'latitude'] = round(round(partial['latitude']/5, 3)*5, 4)
+    pbar.update(1)
+    partial.loc[:, 'longitude'] = round(round(partial['longitude']/5, 3)*5, 4)
+    pbar.update(1)
+
+    f = open(os.path.join(filedir, "geo_model.csv"), "w")
+    np.savetxt(f, partial, fmt=','.join(['%i'] + ['%1.4f']*2))
+    f.close()
+    pbar.update(1)
+
+    pbar.close()
 
 
 def main():
-    print("Generating geo map model data")
-    generate_geo_map(FILE_DIR, DATAFILE)
-    print("Done.")
+    generate_geo_data(FILE_DIR, DATAFILE)
 
 
 if __name__ == '__main__':
