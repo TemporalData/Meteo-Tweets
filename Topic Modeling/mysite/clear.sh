1723d96afcb3ef16c3506465ef7ca059cc98271c
TemporalData <63362559+TemporalData@users.noreply.github.com>
2021-04-11 18:54:24 +0200

    Removal of old build

--- Topic Modeling/mysite/clear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# $bash clear.sh
-python manage.py makemigrations
-python manage.py migrate --run-syncdb
-python manage.py migrate
-python manage.py runserver
--- Topic Modeling/mysite/text/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Document, RegionTopic, DocWeatherEvent
-
-admin.site.register(Document)
-admin.site.register(RegionTopic)
-admin.site.register(DocWeatherEvent)
